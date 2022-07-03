c650bbcff87f72cfec9acdf24217374abdeac919
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-03 11:55:18 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route path="/profile/edit" render={ (props) => <ProfileEdit { ...props } /> } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
+        <Route path="/search" component={ Search } />
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
---
+++ src/pages/Search.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="Search" data-testid="page-search" />
+    );
+  }
+}
+export default Search;
