da2a0dccce1ca05ab51b1c5b5d5252d2f100fd16
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-03 14:43:46 -0300

    Renderizando no app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -8,18 +8,69 @@ import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      username: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { username } = this.state;
+
     return (
-      <Switch>
-        <Route path="/profile/edit" render={ (props) => <ProfileEdit { ...props } /> } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
-        <Route path="/search" component={ Search } />
-        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
-        <Route component={ NotFound } />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route
+            path="/profile/edit"
+            render={ () => <ProfileEdit username={ username } /> }
+          />
+          <Route
+            exact
+            path="/profile"
+            render={ () => <Profile username={ username } /> }
+          />
+          <Route
+            path="/favorites"
+            render={ () => (<Favorites username={ username } />) }
+          />
+          <Route
+            path="/album/:id"
+            render={ (props) => (
+              <Album
+                { ...props }
+                username={ username }
+              />
+            ) }
+          />
+          <Route path="/search" render={ () => <Search username={ username } /> } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Login
+                { ...props }
+                username={ username }
+                handleChange={ this.handleChange }
+              />) }
+          />
+          <Route path="*">
+            {' '}
+            <NotFound />
+            {' '}
+          </Route>
+        </Switch>
+      </Router>
+
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,103 @@
 import React, { Component } from 'react';
+import { string, func, shape } from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.onInputChance = this.onInputChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
+
+    this.state = {
+      submitIsDisabled: true,
+      loading: false,
+    };
+  }
+
+  onButtonClick(event) {
+    event.preventDefault();
+
+    const { username, history: { push } } = this.props;
+
+    this.setState(
+      { loading: true },
+      async () => {
+        await createUser({ name: username });
+        push('/search');
+      },
+    );
+  }
+
+  onInputChange(event) {
+    const { target: { value } } = event;
+    const { handleChange } = this.props;
+
+    const hasValidValue = value.trim().length > 2;
+    this.setState({ submitIsDisabled: !hasValidValue });
+    handleChange(event);
+  }
+
   render() {
+    const { loading, submitIsDisabled } = this.setState;
+    const { username } = this.props;
+
     return (
-      <div data-testid="page-login" />
+      <div>
+        { loading }
+        <form data-testid="page-login">
+          <div>
+            <h1>
+              Trybe
+              <span>Tunes</span>
+            </h1>
+          </div>
+          <label htmlFor="name">
+            <input
+              type="text"
+              placeholder="Seu nome de Usuário"
+              name="username"
+              onChange={ this.onInputChance }
+              data-testid="login-name-input"
+              value={ username }
+            />
+          </label>
+
+          <label htmlFor="passaword">
+            Senha:
+            <input
+              name="passaword"
+              placeholder="Digite sua senha"
+              type="password"
+            />
+          </label>
+
+          <label htmlFor="remember">
+            <input
+              id="remember"
+              name="remember"
+              type="checkbox"
+            />
+          </label>
+
+          <button
+            type="submit"
+            disabled={ submitIsdisabled }
+            onClick={ this.onButtonClick }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propsTypes = {
+  history: shape({}).isRequired,
+  username: string.isRequired,
+  handleChange: func.isRequired,
+};
+
 export default Login;
